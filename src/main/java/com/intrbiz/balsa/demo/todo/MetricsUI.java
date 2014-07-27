8597065e561446ecdef60928769f86e901cbc7cb
Chris Ellis <chris@intrbiz.com>
2014-07-27 15:11:02 +0100

    Move to new metrics API

--- src/main/java/com/intrbiz/balsa/demo/todo/MetricsUI.java
+++ src/main/java/com/intrbiz/balsa/demo/todo/MetricsUI.java
@@ -3,31 +3,31 @@ package com.intrbiz.balsa.demo.todo;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.SortedMap;
 
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.Timer;
 import com.intrbiz.balsa.engine.route.Router;
+import com.intrbiz.gerald.source.IntelligenceSource;
+import com.intrbiz.gerald.witchcraft.Witchcraft;
 import com.intrbiz.metadata.Any;
 import com.intrbiz.metadata.Prefix;
-import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.Metric;
-import com.yammer.metrics.core.MetricName;
-import com.yammer.metrics.core.Timer;
 
 @Prefix("/")
-public class MetricsUI extends Router
+public class MetricsUI extends Router<App>
 {
     @Any("/metrics")
     public void metrics()
     {
         List<MetricDisplay> metrics = model("metrics", new LinkedList<MetricDisplay>());
-        for (Entry<String, SortedMap<MetricName, Metric>> group : Metrics.defaultRegistry().groupedMetrics().entrySet())
+        for (IntelligenceSource source : Witchcraft.get().getSources())
         {
-            for (Entry<MetricName, Metric> metric : group.getValue().entrySet())
+            for (Entry<String, Metric> metric : source.getRegistry().getMetrics().entrySet())
             {
-                if (metric.getValue() instanceof Timer)
-                {
-                    metrics.add(new MetricDisplay(group.getKey(), metric.getKey().getName(), metric.getKey().getScope(), metric.getValue()));
-                }
+                metrics.add(new MetricDisplay(source.getName(), metric.getKey(), metric.getValue()));
             }
         }
         encode("metrics");
@@ -35,30 +35,22 @@ public class MetricsUI extends Router
 
     public static class MetricDisplay
     {
-        private final String group;
-
+        private final String source;
+        
         private final String name;
-
-        private final String scope;
 
         private final Metric metric;
 
-        public MetricDisplay(String group, String name, String scope, Metric metric)
+        public MetricDisplay(String source, String name, Metric metric)
         {
-            this.group = group;
+            this.source = source;
             this.name = name;
-            this.scope = scope;
             this.metric = metric;
         }
-
-        public String getGroup()
+        
+        public String getSource()
         {
-            return group;
-        }
-
-        public String getScope()
-        {
-            return scope;
+            return source;
         }
 
         public String getName()
@@ -69,6 +61,36 @@ public class MetricsUI extends Router
         public Metric getMetric()
         {
             return metric;
+        }
+        
+        public String getType()
+        {
+            return this.metric.getClass().getSimpleName();
+        }
+        
+        public boolean isTimer()
+        {
+            return this.metric instanceof Timer;
+        }
+        
+        public boolean isCounter()
+        {
+            return this.metric instanceof Counter;
+        }
+        
+        public boolean isGauge()
+        {
+            return this.metric instanceof Gauge;
+        }
+        
+        public boolean isMeter()
+        {
+            return this.metric instanceof Meter;
+        }
+        
+        public boolean isHistogram()
+        {
+            return this.metric instanceof Histogram;
         }
     }
 }
